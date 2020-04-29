2851e57774018217013f5b85a339c6f8a1e6de14
mitochondrion <mitochondrion@users.noreply.github.com>
2020-04-29 02:06:08 -0400

    Fixes install script to check for file before moving

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-set -e 
+set -e
 set -o pipefail
 
 cd "${0%/*}"
 
-# Move and save existing configs
 NOW=`date "+%Y%m%d%H%M%S"`
 
-# On Linux you can add the "mv --backup=t" flag for even more safety, but it's not available on OSX :(
-mv ~/.bash_profile ~/.bash_profile.$NOW
-mv ~/.vimrc ~/.vimrc.$NOW
-mv ~/.gitconfig ~/.gitconfig.$NOW
-mv ~/.gitignore ~/.gitignore.$NOW
-mv ~/.ctags ~/.ctags.$NOW
+for DOTFILE in .bash_profile .vimrc .gitconfig .gitignore .ctags
+do
+    # Move existing configs out of the way and add timestamp
+    # On Linux you can add the "mv --backup=t" flag for even more safety, but it's not available on OSX :(
+    [ -f i~/$DOTFILE ] && mv ~/$DOTFILE ~/$DOTFILE.$NOW
 
-# Create symlinks to repository dotfiles
-ln -s ${PWD}/.bash_profile ~/.bash_profile
-ln -s ${PWD}/.vimrc ~/.vimrc
-ln -s ${PWD}/.gitconfig ~/.gitconfig
-ln -s ${PWD}/.gitignore ~/.gitignore
-ln -s ${PWD}/.ctags ~/.ctags
+    # Create symlinks to repository dotfiles
+    ln -s $PWD/$DOTFILE ~/$DOTFILE
+done
 
 # Install Git completion from the official Git repo
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
